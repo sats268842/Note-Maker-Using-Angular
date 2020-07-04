09dfaaf96804411f8011ba19ba8f73d8f9c7eab3
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-04 18:25:32 +0530

    Added Animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +23,8 @@ import { FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -2,11 +2,64 @@ import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim',[
+      transition('void => *',[
+
+        style(
+          {
+            height: 0,
+            opacity: 0,
+            transform: 'scale(0.85)',
+            'margin-bottom' : 0,
+
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingRight: 0,
+            paddingLeft: 0,
+          }),
+          // spacing
+          animate('50ms', style({
+            height: '*',
+            'margin-bottom' : '*',
+            paddingTop: '*',
+            paddingBottom: '*',
+            paddingRight: '*',
+            paddingLeft: '*',
+          })),
+          animate(100)
+      ]),
+
+      transition('* => void',[
+
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0,
+          height: 0,
+          'margin-bottom' : 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }))
+      ]),
+
+
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
