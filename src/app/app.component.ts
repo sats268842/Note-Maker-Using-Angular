17d63bd3dc319e19157a8e486f7ed54038825d28
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-20 12:04:17 +0530

    Push Notificaation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker'
+import { PushNotificationService } from './shared/push-notification.service';
 
+const VAPID_PUBLIC = 'BAdnh6Fd3yHF0HQucQgo1qm9A9flPT_XurCGiByJoTvdQes76IXZK4IVOSgdwDTJnIvKFIsGJ02FBA672Bq1Wz0';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'noteapp';
+
+
+  constructor(swPush: SwPush, pushService: PushNotificationService) {
+    if (swPush.isEnabled) {
+      swPush
+        .requestSubscription({
+          serverPublicKey: VAPID_PUBLIC,
+        })
+        .then(subscription => {
+          pushService.sendSubscriptionToTheServer(subscription).subscribe()
+        })
+        .catch(console.error)
+    }
+  }
+
 }
